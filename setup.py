79dfa10feacf20296e92e9779bdfb267bdece1ac
eliaseriksson <eliaseriksson95@gmail.com>
2020-09-06 00:54:29 +0200

    testing

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="helloworld",
     version="1.0",
-    packages=find_packages(),
-    package_dir={"helloworld": "HelloWorld"}
+    packages=["helloworld"],
 )
